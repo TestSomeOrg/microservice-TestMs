8f191958214e3210e0feb552995cf523a69c6a38
mallika-mewar <m@ne1013>
2017-09-22 12:19:44 +0200

    Generated microservice version 1.0

---
+++ db/TestMs_create_tables.sql
@@ -0,0 +1,17 @@
+--
+-- Database Schema:  commedit
+-- Automatically generated sql script for the service TestMs, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table title.
+--
+CREATE TABLE commedit.title (
+  singer VARCHAR(255) ,
+  title VARCHAR(255) ,
+CONSTRAINT title_PK PRIMARY KEY (title)
+ 
+);
+
+
+
